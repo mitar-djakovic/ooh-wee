1ef7e8e62593454d276baca62f7c90a461eb1680
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2022-08-20 00:30:04 +0100

    App have extended theme configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import { ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
+import { GlobalStyles } from '../styles';
 import { theme } from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyles} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,6 +3,50 @@ import { Theme } from '@emotion/react';
 declare module '@emotion/react' {
   export interface Theme {
     colors: {
+      primary: {
+        dark: string;
+        main: string;
+        hover: string;
+        light: string;
+        focus: string;
+      };
+      success: {
+        dark: string;
+        main: string;
+        hover: string;
+        light: string;
+        focus: string;
+      };
+      warning: {
+        dark: string;
+        main: string;
+        hover: string;
+        light: string;
+        focus: string;
+      };
+      error: {
+        dark: string;
+        main: string;
+        hover: string;
+        light: string;
+        focus: string;
+      };
+      text: {
+        dark: string;
+        main: string;
+        hover: string;
+        light: string;
+        focus: string;
+      };
+      background: {
+        dark: string;
+        main: string;
+        hover: string;
+        light: string;
+        focus: string;
+      };
+    };
+    fonts: {
       primary: string;
     };
   }
@@ -10,6 +54,50 @@ declare module '@emotion/react' {
 
 export const theme: Theme = {
   colors: {
-    primary: 'hotpink',
+    primary: {
+      dark: '#004FC4',
+      main: '#0063F7',
+      hover: '#5B8DEF',
+      light: '#9DBFF9',
+      focus: '#E5F0FF',
+    },
+    success: {
+      dark: '#05A660',
+      main: '#06C270',
+      hover: '#39D98A',
+      light: '#57EBA1',
+      focus: '#E3FFF1',
+    },
+    warning: {
+      dark: '#E57A00',
+      main: '#FF8800',
+      hover: '#FDAC42',
+      light: '#FCCC75',
+      focus: '#FFF8E5',
+    },
+    error: {
+      dark: '#E53535',
+      main: '#FF3B3B',
+      hover: '#FF5C5C',
+      light: '#FF8080',
+      focus: '#FFE5E5',
+    },
+    text: {
+      dark: '#E4E4EB',
+      main: '#FFFFFF',
+      hover: '#EBEBF0',
+      light: '#F2F2F5',
+      focus: '#FAFAFC',
+    },
+    background: {
+      dark: '#1C1C28',
+      main: '#28293D',
+      hover: '#555770',
+      light: '#8F90A6',
+      focus: '#C7C9D9',
+    },
+  },
+  fonts: {
+    primary: 'Inter, sans-serif',
   },
 };
