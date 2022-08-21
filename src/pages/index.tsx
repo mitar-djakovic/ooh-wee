9da1f7bb93e21b0676b31bd5126444976baad7ca
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2022-08-21 23:51:29 +0100

    Created basic button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import styled from 'styled-components';
-
-const Button = styled.button`
-  background-color: ${(props) => props.theme.colors.primary.main};
-`;
+import { Button } from '../components';
 
 const Home = () => {
-  return <Button>Hello</Button>;
+  return <Button label={'Hello'} />;
 };
 
 export default Home;
