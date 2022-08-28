7b3a4006f481e5f408695fa2f3b58e27c09ca304
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2022-08-28 23:24:33 +0100

    Auth pages can be switched

--- src/modules/signin/WelcomeSection/index.tsx
+++ src/modules/signin/WelcomeSection/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import { registrationBg } from '../../../assets/images';
 import { Button } from '../../../components';
@@ -12,21 +13,26 @@ import {
   WelcomeSectionView,
 } from './WelcomeSection.styled';
 
-const WelcomeSection = () => (
-  <WelcomeSectionView>
-    <BackgroundImage>
-      <Image src={registrationBg} alt="Registration background" />
-    </BackgroundImage>
-    <ContentContainer>
-      <Title variant="h1">
-        A highly-curated platform for creating, collecting Comics
-      </Title>
-      <MessageContainer>
-        <SubTitle variant="p">You don’t have an account?</SubTitle>
-        <Button>Sign Up</Button>
-      </MessageContainer>
-    </ContentContainer>
-  </WelcomeSectionView>
-);
+const WelcomeSection = () => {
+  const router = useRouter();
+  const onClick = () => router.push('/signup');
+
+  return (
+    <WelcomeSectionView>
+      <BackgroundImage>
+        <Image src={registrationBg} alt="Registration background" />
+      </BackgroundImage>
+      <ContentContainer>
+        <Title variant="h1">
+          A highly-curated platform for creating, collecting Comics
+        </Title>
+        <MessageContainer>
+          <SubTitle variant="p">You don’t have an account?</SubTitle>
+          <Button onClick={onClick}>Sign Up</Button>
+        </MessageContainer>
+      </ContentContainer>
+    </WelcomeSectionView>
+  );
+};
 
 export default WelcomeSection;
--- src/modules/signup/WelcomeSection/index.tsx
+++ src/modules/signup/WelcomeSection/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import { registrationBg } from '../../../assets/images';
 import { Button } from '../../../components';
@@ -12,21 +13,26 @@ import {
   WelcomeSectionView,
 } from './WelcomeSection.styled';
 
-const WelcomeSection = () => (
-  <WelcomeSectionView>
-    <BackgroundImage>
-      <Image src={registrationBg} alt="Registration background" />
-    </BackgroundImage>
-    <ContentContainer>
-      <Title variant="h1">
-        A highly-curated platform for creating, collecting Comics
-      </Title>
-      <MessageContainer>
-        <SubTitle variant="p">You don’t have an account?</SubTitle>
-        <Button>Sign Up</Button>
-      </MessageContainer>
-    </ContentContainer>
-  </WelcomeSectionView>
-);
+const WelcomeSection = () => {
+  const router = useRouter();
+  const onClick = () => router.push('/signin');
+
+  return (
+    <WelcomeSectionView>
+      <BackgroundImage>
+        <Image src={registrationBg} alt="Registration background" />
+      </BackgroundImage>
+      <ContentContainer>
+        <Title variant="h1">
+          A highly-curated platform for creating, collecting Comics
+        </Title>
+        <MessageContainer>
+          <SubTitle variant="p">You already have an account?</SubTitle>
+          <Button onClick={onClick}>Sign In</Button>
+        </MessageContainer>
+      </ContentContainer>
+    </WelcomeSectionView>
+  );
+};
 
 export default WelcomeSection;
