58f562441258517960856bc73dab60a986b48749
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2023-01-10 01:05:19 +0100

    Add layout interface

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import { ReactNode } from 'react';
 
-export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
-}: {
+interface RootLayoutProps {
   children: ReactNode;
-}) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div>Layout</div>
+        {children}
+      </body>
     </html>
   );
 }
